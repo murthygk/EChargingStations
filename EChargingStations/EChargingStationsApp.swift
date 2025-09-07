8a9d6bab1d0c64a148e29be78e2a0e51c12dbe03
Gudipati, Krishna <krishnam.gudpati@gmail.com>
2025-09-07 16:13:43 -0400

    Initial Commit

---
+++ EChargingStations/EChargingStationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  EChargingStationsApp.swift
+//  EChargingStations
+//
+//  Created by Gudipati, Krishna on 9/7/25.
+//
+
+import SwiftUI
+
+@main
+struct EChargingStationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EChargingStationsTests/EChargingStationsTests.swift
@@ -0,0 +1,17 @@
+//
+//  EChargingStationsTests.swift
+//  EChargingStationsTests
+//
+//  Created by Gudipati, Krishna on 9/7/25.
+//
+
+import Testing
+@testable import EChargingStations
+
+struct EChargingStationsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
